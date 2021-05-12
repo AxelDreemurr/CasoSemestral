4b9b119983faff34daf51f7289bb8951bbcec54f
ZumoshiBlade <jorgetapia0528@gmail.com>
2021-05-12 17:29:45 -0400

    versión 2.4

---
+++ parte 2/Flores/app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import index, about, login
+
+urlpatterns = [
+    path('', index, name="index"),
+    path('about', about, name="about"),
+    path('login', login, name="login"),
+]
---
+++ parte 2/Flores/app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'app/index.html')
+
+def about(request):
+    return render(request, 'app/about.html')
+
+def login(request):
+    return render(request, 'app/login.html')
